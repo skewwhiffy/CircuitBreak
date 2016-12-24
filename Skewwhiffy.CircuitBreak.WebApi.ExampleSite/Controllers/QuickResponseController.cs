d2f0e4272088e1bd660e76c894a6a150eb58a0b2
Kenny Hung <kenny.hung@just-eat.com>
2016-12-24 17:31:19 +0000

    Web API example project, class library, and test folder

---
+++ Skewwhiffy.CircuitBreak.WebApi.ExampleSite/Controllers/QuickResponseController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Skewwhiffy.CircuitBreak.WebApi.ExampleSite.Controllers
+{
+    public class QuickResponseController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ Skewwhiffy.CircuitBreak.WebApi.ExampleSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Skewwhiffy.CircuitBreak.WebApi.ExampleSite.WebApiApplication" Language="C#" %>
