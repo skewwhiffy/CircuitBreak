60908b222c9ffc2bc420633a95c74bbefcaccac8
Kenny Hung <kenny.hung@just-eat.com>
2017-01-03 10:20:37 +0000

    Mad, I tell you

---
+++ Skewwhiffy.CircuitBreak.Tests/Sandbox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Skewwhiffy.CircuitBreak.Tests
+{
+    public class Sandbox
+    {
+        [Test]
+        public async Task CheckStopwatchVsTaskDelay()
+        {
+            var tasks = Enumerable
+                .Range(0, 100)
+                .Select(i => CheckSingle())
+                .ToArray();
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task CheckSingle()
+        {
+            var sw = Stopwatch.StartNew();
+            await Task.Delay(TimeSpan.FromSeconds(6));
+            sw.Stop();
+            Console.Write($"{sw.Elapsed}");
+        }
+    }
+}
